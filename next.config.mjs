c134335729471d6673de4be9ffe1c0ec27fa4214
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-11-12 17:51:29 +0500

    Updated next.config.mjs for deployment settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'standalone', // Helps Vercel with optimized builds
+    reactStrictMode: true, // Enables strict mode for React
+    trailingSlash: false, // Adjust if you want URLs to end with slashes
+    experimental: {
+      appDir: true, // Ensures the use of the /app directory routing
+    },
+  };
+  
+  export default nextConfig;
+  
